0060de87571d974534e5a0098f29141b17575c88
Rafael Lima <Rafael.LA.Lima@ctw.bmwgroup.com>
2020-02-04 21:08:21 +0000

    Project 1: WeSplit

---
+++ WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by CTW00710-Admin on 04/02/2020.
+//  Copyright © 2020 RLima. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
